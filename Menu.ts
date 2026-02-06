d8bd872899371728a27d5bd7535fdc90a3c92454
outwake <larissa.mendy@gmail.com>
2026-02-06 19:15:10 -0300

    :ok_hand: Ajustes no Menu

--- Menu.ts
+++ Menu.ts
@@ -1,7 +1,17 @@
 import { colors } from "./src/util/Colors";
 import { Input } from "./src/util/Input";
 import { Produto } from "./src/model/Produto";
+import { Medicamento } from "./src/model/Medicamento";
+import { Cosmetico } from "./src/model/Cosmetico";
+import { ProdutoController } from "./src/Controller/ProdutoController";
+import { question, questionInt } from "readline-sync";
 
+
+//criar objeto global da classe Produto controller
+const produtos = new ProdutoController();
+
+//criar Array contendo os tipos de produtos
+const tiposProdutos = ['Medicamento', `Cosméticos`]
 
 export function main(){
 
@@ -32,7 +42,8 @@ colors.reset);
             console.log("=======================================")
             console.log("||          CRIAR PRODUTO            ||")
             console.log("=======================================")
-
+            criarProduto();
+            keyPress();
         break;
 
         case 2:
@@ -40,6 +51,8 @@ colors.reset);
             console.log("=======================================")
             console.log("||       LISTAR TODOS PRODUTO        ||")
             console.log("=======================================")
+            produtos.listarTodas();
+            keyPress();
 
         break;
 
@@ -48,6 +61,8 @@ colors.reset);
             console.log("=======================================")
             console.log("||           BUSCAR PRODUTO          ||")
             console.log("=======================================")
+            buscarProduto();
+            keyPress();
 
         break;
         case 4:
@@ -55,6 +70,7 @@ colors.reset);
             console.log("=======================================")
             console.log("||         ATUALIZAR PRODUTO         ||")
             console.log("=======================================")
+            atualizarProduto();
          
 
         break;
@@ -63,6 +79,7 @@ colors.reset);
             console.log("=======================================")
             console.log("||          APAGAR PRODUTO           ||")
             console.log("=======================================")
+            deletarProduto();
 
 
         break;
@@ -78,6 +95,112 @@ colors.reset);
   
 
 }
+
+
+        //Opção 1: Criar um novo produto
+        function criarProduto(){
+            //Nome PRODUTO
+            console.log("Digite o Nome do Produto")
+            const nomeProduto = Input.question("")
+            
+            //Preço do Produto
+            console.log("Digite o Preço do Produto")
+            const precoProduto = Input.questionFloat("")
+
+
+            //Definição do tipo de Produto
+
+            console.log("Digite o Tipo de Produto:")
+            const tipo = Input.keyInSelect(tiposProdutos, "", {cancel: false}) + 1;
+
+            //Tipo de Produtos
+
+            switch (tipo){
+
+                case 1:  
+                    const generico = "Medicamento";
+                produtos.cadastrar(new Medicamento(produtos.gerarId(), nomeProduto, tipo, precoProduto, generico ))    
+                break;
+
+                case 2:
+                    const fragancia = "Fragancia";
+                produtos.cadastrar(new Cosmetico(produtos.gerarId(), nomeProduto, tipo, precoProduto, fragancia))
+                break;
+            }
+        }
+            //OPÇÃO 2: Buscar Produtos
+            function buscarProduto(): void{
+            console.log("Digite o Numero do Produto:")
+            const numero = Input.questionInt("")
+                produtos.procurarPorId(numero);
+
+            }
+
+            //OPÇÃO 4: ATUALIZAR PRODUTO
+            function atualizarProduto(): void{
+
+            console.log("Digite o Numero do Produto:")
+            const numero= Input.questionInt("");
+
+            const produto = produtos.buscarNoArray(numero);
+
+            if(produto !== null){
+
+
+            let nomeProduto = produto.nomeProduto;
+            let precoProduto = produto.preco;
+            const tipo = produto.tipo;
+            
+
+            console.log(`Nome do produto atual: ${nomeProduto}`)
+            console.log("Digite o novo nome do Produto: \n (Pressione Enter para manter o valor atual");
+            let entrada = Input.question("");
+            
+            nomeProduto=entrada.trim()===""? nomeProduto:entrada;
+
+
+            console.log(`Preço do produto atual: ${precoProduto}`)
+            console.log("Digite o novo preço do Produto: \n (Pressione Enter para manter o valor atual");
+            entrada = Input.question("");
+            precoProduto = entrada.trim()===""? precoProduto:parseFloat(entrada.replace(",","."))
+
+            
+            console.log("Tipo do produto atual:", tipo)
+            console.log("Digite o Tipo de Produto:")
+            const tipo1 = Input.keyInSelect(tiposProdutos, "", {cancel: false}) + 1;
+            switch(tipo1){
+                    case 1:  
+                    const generico = "Medicamento";
+                produtos.atualizar(new Medicamento(numero, nomeProduto, tipo, precoProduto, generico )) 
+               
+                break;
+
+                case 2:
+                    const fragancia = "Fragancia";
+                produtos.atualizar(new Cosmetico(numero, nomeProduto, tipo, precoProduto, fragancia))
+               
+                break;
+            }
+            }
+
+            }
+
+            
+           //OPÇÃO 5: APAGAR PRODUTO
+
+           function deletarProduto():void{
+            console.log("Digite o ID do Produto")
+            const numero =questionInt("")
+            //let op= Input.question(`Deseja realmente apagar a conta ${numero}? (S) Sim (N) Não `).toUpperCase();
+        
+         //if(op==="S"){
+            produtos.deletar(numero);
+        //}
+       // else if(op ==="N" || op!=="S"){
+
+        return;
+        //}
+           }
 
             function sobre(){
         console.log(colors.bg.black, colors.fg.cyanstrong,
